16350a99758c316eedbd8199eff75a7cc12fd2ee
Patrick Tavares dos Santos <ptsantosrj@gmail.com>
2024-07-17 22:01:02 -0300

    Criado reporistorio para o exercicio sobre Exceptions

---
+++ src/test/java/com/poo/exceptions/ExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo.exceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
